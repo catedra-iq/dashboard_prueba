aebf9cdf48f6e9e66fbbeb39b24120d3e41506ba
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2021-07-27 23:59:06 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+st.title(
+	"Gases Reales"
+)
+
+st.subheader("Cátedra de Introducción a la Química UNLP. FCE")
+
+
+st.title("Murieta")
+
+x=np.linspace(-1,1,100)
+y =x**2
+
+fig,ax=plt.subplots(2,1)
+ax=ax.flatten()
+ax[0].plot(x,y)
+ax[1].scatter(x,y)
+st.pyplot(fig)
+
+
